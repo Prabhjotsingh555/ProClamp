ecd5ceab4f6a070b82f4bfd5829fb2450f918be1
W0828035 <W0828035@ADL-A0214Z-33>
2023-10-10 15:00:24 -0400

    First and Second Step Complete

---
+++ Controllers/ProClampController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProClamp.Controllers
+{
+    public class ProClampController : Controller
+    {
+        // 
+        // GET: /ProClamp/
+
+        public string Index()  //I tested this by typing /ProClamp in the url of page and this worked
+        {
+            return "This is my default action...";
+        }
+
+        //-------------------------------------
+
+        /*// GET: /ProClamp/Welcome/   
+
+        public string Welcome()   //I tested this by typing ProClamp/Welcome in the url of page and this worked
+        {
+            return "This is the Welcome action method...";
+        
+        //now comment this string for the new Welcome string
+        */
+
+        //-------------------------------------
+
+        /*// GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)  //I tested this by typing ProClamp/Welcome?name=Rick&numtimes=4 in the url of page and this worked
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+        //now comment this string for the new Welcome string
+        */
+
+        //-------------------------------------
+
+        public string Welcome(string name, int ID = 1) //I tested this by typing ProClamp/Welcome/3?name=Prabhjot Singh in the url of page and this worked
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+
+    }
+}
--- README.txt
+++ README.txt
@@ -13,10 +13,24 @@ Program author = Prabhjot Singh
 6. Then click on git and make new clone respository 
 7. Write the desciption
 8. Done.
+9. Then create readme
+10. Then comment the local host line  7 inside > Properties > launchSettings.json
+Test it = IT WORKED
+11. Commint to Github
 
 -------COMPLETED FIRST STEP-------
 
-1. Then create readme
-2. Then comment the local host line  7 inside > Properties > launchSettings.json
-Test it = IT WORKED
-3. Commint to Github
+-------START SECOND STEP-------
+
+Starting time = 10-10-2023 0237
+
+1. Right click on Controllers > Add > Controller > MVC Controller - Empty > Add > name it > ProClampController.cs
+2. Make two new string, First > deafult messag, Second > welcome message
+ Test it = IT WORKED
+3. Make another public string code with  name and numTimes variable and comments previous one
+ Test it = IT WORKED
+4. Make another string with name and ID variable and comment previous one
+ Test it = IT WORKED
+5. Commint to Github
+
+-------COMPLETED SECOND STEP-------
